6c04b2ab0d6e7b59895b55647741a7177e52f75f
Guillermo Sepulveda R <guillesep@gmail.com>
2021-10-30 14:23:34 -0300

    feat: llamada a servicio de buda de mercados y retorno lista con mercados con clp

--- cogs/shark.py
+++ cogs/shark.py
@@ -4,17 +4,28 @@ import requests
 from discord.ext import commands
 
 urlMarkets = 'https://www.buda.com/api/v2/markets'
-response = requests.get(urlMarkets)
+responseMarket = requests.get(urlMarkets)
 
 market_id = 'btc-clp' """ response.name """
 urlTicker = f'https://www.buda.com/api/v2/markets/{market_id}/ticker'
-response = requests.get(urlTicker)
+responseTicker = requests.get(urlTicker)
 
 
 
 class Shark(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        
+    @commands.command()
+    async def mercado(self, ctx):
+        responseMarket = requests.get(urlMarkets)
+        response = responseMarket.json()
+        mercado = []
+        for x in response:
+            if x.quote_currency == 'CLP':
+                mercado.append(x.name)
+
+        await ctx.send(f'Mercado: {mercado}')
 
 def setup(bot: commands.Bot):
     bot.add_cog(Shark(bot))
