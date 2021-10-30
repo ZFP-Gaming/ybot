c464f00421656152b0c38c28ae54975ecf3dd5f4
Guillermo Sepulveda R <guillesep@gmail.com>
2021-10-30 14:32:48 -0300

    fix: se elimina llamada al servicio

--- cogs/shark.py
+++ cogs/shark.py
@@ -15,17 +15,6 @@ responseTicker = requests.get(urlTicker)
 class Shark(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        
-    @commands.command()
-    async def mercado(self, ctx):
-        responseMarket = requests.get(urlMarkets)
-        response = responseMarket.json()
-        mercado = []
-        for x in response:
-            if x.quote_currency == 'CLP':
-                mercado.append(x.name)
-
-        await ctx.send(f'Mercado: {mercado}')
 
 def setup(bot: commands.Bot):
     bot.add_cog(Shark(bot))
