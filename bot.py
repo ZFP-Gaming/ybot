fa4d7306785c0758d65a2d1b4e7700004e96d6d7
Ernesto Acevedo <eacevedo@debian.cl>
2021-11-07 14:20:45 -0300

    fix bug in sound effect upload

--- bot.py
+++ bot.py
@@ -799,7 +799,8 @@ async def add(ctx):
         opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
         urllib.request.install_opener(opener)
         urllib.request.urlretrieve(url, filename)
-        shutil.move(filename, 'sounds')
+        shutil.move(filename, 'sound_effects')
+        ctx.message.add_reaction('✅')
     else:
         await ctx.send(ACCESS_DENIED)
 
