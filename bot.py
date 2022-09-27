ef69020a802839e0a2340163ef2af884ccba85ab
Ernesto Acevedo <eacevedo@debian.cl>
2022-09-26 22:22:55 -0300

    refactor

--- bot.py
+++ bot.py
@@ -252,11 +252,12 @@ async def on_voice_state_update(member, before, after):
         await update_nickname(member)
         if check_ban(member.id):
             return
-        if member.id == 134686670535524352:
+        if member.id == vitoco:
             if before.self_mute == False and after.self_mute == True:
-                voice_client.play(discord.FFmpegPCMAudio(f'sound_effects/aplauso.mp3'), after=lambda x: check_queue(voice_client))
-                voice_client.source = discord.PCMVolumeTransformer(voice_client.source)
-                voice_client.source.volume = bot.volume
+                if voice_client and voice_client.channel == after.channel:
+                    voice_client.play(discord.FFmpegPCMAudio(f'sound_effects/aplauso.mp3'), after=lambda x: check_queue(voice_client))
+                    voice_client.source = discord.PCMVolumeTransformer(voice_client.source)
+                    voice_client.source.volume = bot.volume
         if before.channel is None and after.channel is not None and member.bot == False:
             if voice_client and voice_client.channel == after.channel:
                 id = member.id
