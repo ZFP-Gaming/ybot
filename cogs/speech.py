53e5edf68db5c7b333a63c1e6e29c172896c5dc8
Jorge Fuentes <glisterkaiser@gmail.com>
2022-10-31 15:34:41 -0300

    Refactor: Extracting answers in pregunta function for speech cog

--- cogs/speech.py
+++ cogs/speech.py
@@ -2,37 +2,37 @@ import discord
 import random
 from discord.ext import commands
 
-class Speech(commands.Cog):
-    answers = [
-        'En mi opinión, sí',
-        'Es cierto',
-        'Es decididamente así',
-        'Probablemente',
-        'Buen pronóstico',
-        'Todo apunta a que sí',
-        'Sin duda',
-        'Sí',
-        'Sí - definitivamente',
-        'Debes confiar en ello',
-        'Respuesta vaga, vuelve a intentarlo',
-        'Pregunta en otro momento',
-        'Será mejor que no te lo diga ahora',
-        'No puedo predecirlo ahora',
-        'Concéntrate y vuelve a preguntar',
-        'No cuentes con ello',
-        'Mi respuesta es no',
-        'Mis fuentes me dicen que no',
-        'Las perspectivas no son buenas',
-        'Muy dudoso',
-        'Yo no volveria a confiar'
-    ]
+ANSWERS = [
+    'En mi opinión, sí',
+    'Es cierto',
+    'Es decididamente así',
+    'Probablemente',
+    'Buen pronóstico',
+    'Todo apunta a que sí',
+    'Sin duda',
+    'Sí',
+    'Sí - definitivamente',
+    'Debes confiar en ello',
+    'Respuesta vaga, vuelve a intentarlo',
+    'Pregunta en otro momento',
+    'Será mejor que no te lo diga ahora',
+    'No puedo predecirlo ahora',
+    'Concéntrate y vuelve a preguntar',
+    'No cuentes con ello',
+    'Mi respuesta es no',
+    'Mis fuentes me dicen que no',
+    'Las perspectivas no son buenas',
+    'Muy dudoso',
+    'Yo no volveria a confiar'
+]
 
+class Speech(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.command()
     async def pregunta(self, ctx):
-        await ctx.send(random.choice(self.answers))
+        await ctx.send(random.choice(ANSWERS))
 
 def setup(bot: commands.Bot):
     bot.add_cog(Speech(bot))
