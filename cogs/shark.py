8fd88ba8696f29a2337d9376c091db4cf5879c18
Jorge Fuentes <glisterkaiser@gmail.com>
2022-10-31 14:16:49 -0300

    Cleanup: remove unused tabs and spaces

--- cogs/shark.py
+++ cogs/shark.py
@@ -10,12 +10,10 @@ market_id = 'btc-clp' #response.name
 urlTicker = f'https://www.buda.com/api/v2/markets/{market_id}/ticker'
 responseTicker = requests.get(urlTicker)
 
-
-
 class Shark(commands.Cog):
     def __init__(self, bot: commands.Bot):
-        self.bot = bot  
-        
+        self.bot = bot
+
     @commands.command()
     async def mercados(self, ctx):
         responseMarket = requests.get(urlMarkets)
@@ -26,15 +24,15 @@ class Shark(commands.Cog):
                 mercado.append(x.name)
 
         await ctx.send(f'Mercado: {mercado}')
-        
+
     @commands.command()
     async def valor(self, ctx, *, query):
         await ctx.message.add_reaction('🔍')
         urlTicker = f'https://www.buda.com/api/v2/markets/{query}/ticker'
         responseMarket = requests.get(urlTicker)
         response = responseMarket.json()
-        
+
         await ctx.send(f'El ultimo precio fue $**{response.last_price[0]}** **{response.last_price[1]}**')
-            
+
 def setup(bot: commands.Bot):
     bot.add_cog(Shark(bot))
